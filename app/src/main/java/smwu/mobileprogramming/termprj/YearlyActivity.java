7a35c265dbfae2a3607b79bff517b0d76b666a4e
bestlalala <bestdustn@gmail.com>
2021-12-02 23:38:12 +0900

    add yearly card list

--- app/src/main/java/smwu/mobileprogramming/termprj/YearlyActivity.java
+++ app/src/main/java/smwu/mobileprogramming/termprj/YearlyActivity.java
@@ -1,16 +1,21 @@
 package smwu.mobileprogramming.termprj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class YearlyActivity extends AppCompatActivity {
     Button backBtn, nextBtn;
-    Button nextYear, nnextYear;
     FragmentYearly fragmentYearly;
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    YearlyPlanAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,28 @@ public class YearlyActivity extends AppCompatActivity {
         setContentView(R.layout.yearly);
 
         fragmentYearly = new FragmentYearly();
-
         getSupportFragmentManager().beginTransaction().add(R.id.frame, fragmentYearly).commit();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new YearlyPlanAdapter();
+
+        adapter.addItem(new YearlyPlan(2022, "2022년 계획"));
+        adapter.addItem(new YearlyPlan(2023, "2023년 계획"));
+
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnYearlyItemClickListener() {
+            @Override
+            public void onItemClick(YearlyPlanAdapter.ViewHolder holder, View view, int position) {
+                YearlyPlan item = adapter.getItem(position);
+
+                Toast.makeText(getApplicationContext(), "연도 선택됨"+item.getYear(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         backBtn = findViewById(R.id.button_back);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -36,22 +61,6 @@ public class YearlyActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CategoryActivity.class); // MonthlyActivity로 수정해야 함.
                 startActivity(intent);
-            }
-        });
-
-        nextYear = findViewById(R.id.nextYearBtn);
-        nextYear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //2022년 목표 설정하기
-            }
-        });
-
-        nnextYear = findViewById(R.id.nnextYearBtn);
-        nnextYear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //2023년 목표 설정하기
             }
         });
     }
---
+++ app/src/main/java/smwu/mobileprogramming/termprj/YearlyPlanAdapter.java
@@ -0,0 +1,93 @@
+package smwu.mobileprogramming.termprj;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class YearlyPlanAdapter extends RecyclerView.Adapter<YearlyPlanAdapter.ViewHolder> implements OnYearlyItemClickListener {
+    ArrayList<YearlyPlan> items = new ArrayList<YearlyPlan>();
+    OnYearlyItemClickListener listener;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.yearlyplan_item, parent, false);
+
+        return new ViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        YearlyPlan item =items.get(position);
+        holder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void addItem(YearlyPlan item) {
+        items.add(item);
+    }
+
+    public void setItems(ArrayList<YearlyPlan> items) {
+        this.items = items;
+    }
+
+    public YearlyPlan getItem(int position) {
+        return items.get(position);
+    }
+
+    public void setItem(int position, YearlyPlan item) {
+        items.set(position, item);
+    }
+
+    public void setOnItemClickListener(OnYearlyItemClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onItemClick(ViewHolder holder, View view, int position) {
+        if (listener != null) {
+            listener.onItemClick(holder, view, position);
+        }
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView, textView2;
+
+        public ViewHolder(View itemView, final OnYearlyItemClickListener listener) {
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.year);
+            textView2 = itemView.findViewById(R.id.goalText);
+
+            itemView.setOnClickListener(new View.OnClickListener(){ // 아이템 뷰에 OnClickListener 설정하기
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+
+                    if (listener != null) { // 아이템 뷰 클릭 시 미리 정의한 다른 리스너의 메서드 호출하기
+                        listener.onItemClick(ViewHolder.this, view, position);
+                    }
+                }
+            });
+        }
+
+        public void setItem(YearlyPlan item) {
+            textView.setText(String.valueOf(item.getYear()));
+            textView2.setText(item.getGoalText());
+        }
+
+    }
+
+
+}
